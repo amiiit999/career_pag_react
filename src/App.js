7a1d582f6837f4fb63a0f5a3c18fe4dbfedc6ec5
amiiit999 <amiiitshah@gmail.com>
2021-10-14 13:28:28 +0530

    upload user cv

---
+++ src/App.js
@@ -0,0 +1,30 @@
+// import logo from './logo.svg';
+import './App.css';
+// import Form from './components/form/Form.js'
+import CareerForm from './components/CareerForm.js'
+
+
+function App() {
+  return (
+    <div className="App">
+     
+      <CareerForm></CareerForm>
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
+    </div>
+  );
+}
+
+export default App;
